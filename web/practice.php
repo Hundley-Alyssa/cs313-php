24382fe8302fd93a37f07c1049804fda0fbb5e2a
Alyssa Hundley <ahundley11@gmail.com>
2019-01-19 00:20:54 -0700

    practice commit

---
+++ web/practice.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <title>CS 313 | HOME</title>
+    <meta charset="utf-8" />
+    <link href="myhome.css" type="text/css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+</head>
+
+<body>
+
+    
+              
+     <?php include 'header.php'; ?>
+
+
+
+
+   
+
+    <main>
+        <div>
+
+            <nav class="sideNav">
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="assignments.php">ASSIGNMENTS</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+
+        <div id="about">
+
+            <figure>
+                <img src="Hundley_fam-7.jpg" alt="Alyssa Stevenson" title="Alyssa Stevenson" />
+
+                <figcaption>
+                    Hi! I'm Alyssa
+
+                </figcaption>
+
+            </figure>
+
+
+            <p id="aboutMe">MY NAME IS ALYSSA HUNDLEY. I AM ORIGINALLY FROM ARIZONA, BUT I CURRENTLY LIVE IN SPRINGVILLE, UTAH. I HAVE BEEN MARRIED FOR TWO YEARS AND MY HUSBAND AND I RECENTLY HAD A LITTLE BABY GIRL! HER NAME IS JORDY AND SHE IS THE CUTEST LITTLE BABY EVER (IN MY BIASED OPINION). I LOVE BEING A MOM AND I AM VERY EXCITED TO FINISH SCHOOL ONCE THIS SEMESTER IS OVER. IT HAS BEEN A GREAT EXPERIENCE BUT I AM ANXIOUS TO FINISH!</p>
+
+
+
+
+
+
+        </div>
+
+
+    </main>
+
+    <footer>
+        
+        <?php include 'footer.php'; ?>
+        
+    </footer>
+
+</body>
+
+</html>
