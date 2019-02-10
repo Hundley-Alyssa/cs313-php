4e117181c580808c5c836813fb599aa1d8004d9a
Alyssa Hundley <ahundley11@gmail.com>
2019-02-09 23:02:51 -0700

    slkdj

--- web/recipetitViewRecipe.php
+++ web/recipetitViewRecipe.php
@@ -15,9 +15,9 @@ $db = get_db();
 $stmt = $db->prepare('SELECT id, name, cook_time, oven_temp, ingredients, instructions FROM recipe WHERE id=:id');
 $stmt->bindValue(':id', $recipe_id, PDO::PARAM_INT);
 $stmt->execute();
-$recipe_rows = $stmt->fetchAll(PDO::Fetch_ASSOC);
+$recipe_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$recipe_name = $recipe_rows[0]['name'];
+// $recipe_name = $recipe_rows[0]['name'];
 ?>
 
 
